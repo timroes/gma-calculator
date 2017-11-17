5d81c33447442be483b41d5220819be6fbd21e8d
Tim Roes <mail@timroes.de>
2017-11-17 11:32:44 +0100

    Add rates for 2018

--- src/rates/index.js
+++ src/rates/index.js
@@ -1,10 +1,12 @@
 import rates2017 from './2017';
+import rates2018 from './2018';
 
 const rates = {
 	2017: rates2017,
+	2018: rates2018,
 };
 
-const fallbackYear = 2017;
+const fallbackYear = 2018;
 
 export function getRateForCountry(code, year) {
 	if (year in rates) {
